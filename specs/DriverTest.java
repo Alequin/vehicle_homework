95fea3a4557f2e57bb888ab4081ede8768a9ede1
James Cox <j-cox03@hotmail.com>
2017-06-28 19:09:04 +0100

    Made test for getting the Drivable object from a Driver

---
+++ specs/DriverTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import vehicle.*;
+
+public class DriverTest{
+
+  Dodgem dodgem;
+  QuadBike quadBike;
+  Driver driver;
+
+  @Before
+  public void setup(){
+    dodgem = new Dodgem(2);
+    quadBike = new QuadBike(100);
+    driver = new Driver("Mia", dodgem);
+  }
+
+  @Test
+  public void canGetCurrentRide(){
+    Drivable ride = driver.getCurrentDrivable();
+    Dodgem dodgem = (Dodgem) ride;
+    assertEquals(2, dodgem.numberOfSeats());
+  }
+
+}
