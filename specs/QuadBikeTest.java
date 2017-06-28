082a00a436ce0676a72eb22d5dd04217370664bf
James Cox <j-cox03@hotmail.com>
2017-06-28 18:30:04 +0100

    Corrected mis-typed method named in QuadBikeTest

--- specs/QuadBikeTest.java
+++ specs/QuadBikeTest.java
@@ -14,14 +14,14 @@ public class QuadBikeTest{
 
   @Test
   public void canGetEngineCapacity(){
-    assertEquals(100, quadBike.getNumberOfSeats());
+    assertEquals(100, quadBike.getEngineCapacity());
   }
 
   @Test
   public void canCorrectEngineCapacity(){
     quadBike = new QuadBike(48);
-    assertEquals(49, quadBike.getNumberOfSeats());
+    assertEquals(49, quadBike.getEngineCapacity());
     quadBike = new QuadBike(1001);
-    assertEquals(1000, quadBike.getNumberOfSeats());
+    assertEquals(1000, quadBike.getEngineCapacity());
   }
 }
