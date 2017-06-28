58389444ee250d59443f14bf95b8ab86c9ff14f3
James Cox <j-cox03@hotmail.com>
2017-06-28 18:08:29 +0100

    Moved DodgemTest out of file Vehicle and into the specs file to make sure it works with the ruby file run_tests

---
+++ specs/DodgemTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import vehicle.Dodgem;
+
+public class DodgemTest{
+
+  Dodgem dodgem;
+
+  @Before
+  public void setup(){
+    dodgem = new Dodgem(2);
+  }
+
+  @Test
+  public void canGetNumberOfSeats(){
+    assertEquals(2, dodgem.getNumberOfSeats());
+  }
+
+  @Test
+  public void canCorrectNumberOfSeats(){
+    dodgem = new Dodgem(-1);
+    assertEquals(1, dodgem.getNumberOfSeats());
+    dodgem = new Dodgem(4);
+    assertEquals(3, dodgem.getNumberOfSeats());
+  }
+}
