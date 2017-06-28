9b771848e70fc3518257a36a70a30e9edcc65801
James Cox <j-cox03@hotmail.com>
2017-06-28 17:33:04 +0100

    Created constructors for Dogem and QuadBike classes which call super to set averageSpeed

--- vehicle/Dodgem.java
+++ vehicle/Dodgem.java
@@ -1,5 +1,9 @@
 public class Dodgem extends Vehicle implements Drivable{
 
+  public Dodgem(){
+    super(2);
+  }
+
   public int driveDistance(int distance){
     return 0;
   }
--- vehicle/QuadBike.java
+++ vehicle/QuadBike.java
@@ -1,5 +1,9 @@
 public class QuadBike extends Vehicle implements Drivable{
 
+  public QuadBike(){
+    super(15);
+  }
+
   public int driveDistance(int distance){
     return 0;
   }
