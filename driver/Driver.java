59a6127ae1f35212885aa56164ccb8e332e8a921
James Cox <j-cox03@hotmail.com>
2017-06-28 19:02:19 +0100

    Made Driver class outline

---
+++ driver/Driver.java
@@ -0,0 +1,15 @@
+package driver;
+
+import Vehicle.*;
+
+public class Driver{
+
+  private String name;
+  private Drivable ride;
+
+  public Driver(String name, Drivable ride){
+    this.name = name;
+    this.ride = ride;
+  }
+
+}
